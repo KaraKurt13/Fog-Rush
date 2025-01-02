c92aed57df49368b8867a025773dd523d5244120
KaraKurt <penguin.ners@gmail.com>
2025-01-02 12:54:49 +0200

    Facebook ads settings

--- Assets/Scripts/SDK/FacebookAdsManager.cs
+++ Assets/Scripts/SDK/FacebookAdsManager.cs
@@ -1,4 +1,5 @@
 using AudienceNetwork;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,16 @@ namespace Assets.Scripts.Facebook
 
         [SerializeField] GameObject _adObject;
 
+        private void Start()
+        {
+            AdSettings.AddTestDevice("ee824333-21fb-423e-8c26-26beb6617faf");
+            AdSettings.SetMixedAudience(true);
+            AdSettings.SetDataProcessingOptions(new String[] { "LDU" }, 0, 0);
+            _adView = new AdView("2311328639234573_2311329229234514", AdSize.BANNER_HEIGHT_50);
+            _adView.Register(_adObject);
+            _adView.LoadAd();
+
+        }
         /*private void Start()
         {
             /*AdSettings.AddTestDevice("ee824333-21fb-423e-8c26-26beb6617faf");
